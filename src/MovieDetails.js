f7a9ef63227f3cc1a44a9ee275eb7ff839981656
Ahmed-K-Fouda <mido203966@gmail.com>
2024-07-13 23:31:05 +0300

    fix some err

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -66,7 +66,7 @@ export function MovieDetails({
         try {
           setIsLoading(true);
           const response = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+            `https://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
           );
           const data = await response.json();
           setMovie(data);
