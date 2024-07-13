f8d4f27834e139d773a51108dfa26da84764269b
Ahmed-K-Fouda <mido203966@gmail.com>
2024-07-13 23:26:39 +0300

    fix some err

--- src/useMovies.js
+++ src/useMovies.js
@@ -18,7 +18,7 @@ export function useMovies(query) {
         try {
           setError("");
           const response = await fetch(
-            `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+            `https://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
             { signal: controllerRef.current.signal }
           );
           if (!response.ok)
