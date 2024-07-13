da2dc09fc5ca254c116f292933b6d17f7c9a199a
Ahmed-K-Fouda <mido203966@gmail.com>
2024-07-13 22:12:26 +0300

    comment test() on index.js file

--- src/index.js
+++ src/index.js
@@ -6,16 +6,16 @@ import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StarRating maxRating={5} onMovieRating={setMovieRating}></StarRating>
-      <p>This movie got {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StarRating maxRating={5} onMovieRating={setMovieRating}></StarRating>
+//       <p>This movie got {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 root.render(
   <React.StrictMode>
